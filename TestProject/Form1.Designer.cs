1b19d6a91246d489d4d2bc59ab357a8434b5d3c9
zayarniy <zaazaa@yandex.ru>
2019-08-16 03:07:56 +0300

    Не значительные изменения

--- TestProject/Form1.Designer.cs
+++ TestProject/Form1.Designer.cs
@@ -33,13 +33,11 @@
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.tsmiSetFolder = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiSave = new System.Windows.Forms.ToolStripMenuItem();
             this.tsmiLogShow = new System.Windows.Forms.ToolStripMenuItem();
             this.tsmiClearLogFile = new System.Windows.Forms.ToolStripMenuItem();
-            this.tsmiSave = new System.Windows.Forms.ToolStripMenuItem();
             this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
             this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
-            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
             this.statusStrip1 = new System.Windows.Forms.StatusStrip();
             this.tsslTimeSpan = new System.Windows.Forms.ToolStripStatusLabel();
             this.tsslStatus = new System.Windows.Forms.ToolStripStatusLabel();
@@ -61,7 +59,6 @@
             this.cbAllDirectories = new System.Windows.Forms.CheckBox();
             this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
             this.menuStrip1.SuspendLayout();
-            this.toolStrip1.SuspendLayout();
             this.statusStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
             this.splitContainer1.Panel1.SuspendLayout();
@@ -73,11 +70,13 @@
             // 
             // menuStrip1
             // 
+            this.menuStrip1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.fileToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(1023, 24);
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(9, 3, 0, 3);
+            this.menuStrip1.Size = new System.Drawing.Size(1534, 31);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -91,64 +90,48 @@
             this.toolStripMenuItem1,
             this.exitToolStripMenuItem});
             this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
-            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(46, 25);
             this.fileToolStripMenuItem.Text = "File";
             // 
             // tsmiSetFolder
             // 
             this.tsmiSetFolder.Name = "tsmiSetFolder";
-            this.tsmiSetFolder.Size = new System.Drawing.Size(180, 22);
+            this.tsmiSetFolder.Size = new System.Drawing.Size(147, 26);
             this.tsmiSetFolder.Text = "Set folder";
             this.tsmiSetFolder.Click += new System.EventHandler(this.tsmiSetFolder_Click);
+            // 
+            // tsmiSave
+            // 
+            this.tsmiSave.Name = "tsmiSave";
+            this.tsmiSave.Size = new System.Drawing.Size(147, 26);
+            this.tsmiSave.Text = "Save";
+            this.tsmiSave.Click += new System.EventHandler(this.tsmiSave_Click);
             // 
             // tsmiLogShow
             // 
             this.tsmiLogShow.Name = "tsmiLogShow";
-            this.tsmiLogShow.Size = new System.Drawing.Size(180, 22);
+            this.tsmiLogShow.Size = new System.Drawing.Size(147, 26);
             this.tsmiLogShow.Text = "Show log";
             this.tsmiLogShow.Click += new System.EventHandler(this.tsmiLogShow_Click);
             // 
             // tsmiClearLogFile
             // 
             this.tsmiClearLogFile.Name = "tsmiClearLogFile";
-            this.tsmiClearLogFile.Size = new System.Drawing.Size(180, 22);
+            this.tsmiClearLogFile.Size = new System.Drawing.Size(147, 26);
             this.tsmiClearLogFile.Text = "Clear log";
             this.tsmiClearLogFile.Click += new System.EventHandler(this.tsmiClearLogFile_Click);
-            // 
-            // tsmiSave
-            // 
-            this.tsmiSave.Name = "tsmiSave";
-            this.tsmiSave.Size = new System.Drawing.Size(180, 22);
-            this.tsmiSave.Text = "Save";
-            this.tsmiSave.Click += new System.EventHandler(this.tsmiSave_Click);
             // 
             // toolStripMenuItem1
             // 
             this.toolStripMenuItem1.Name = "toolStripMenuItem1";
-            this.toolStripMenuItem1.Size = new System.Drawing.Size(177, 6);
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(144, 6);
             // 
             // exitToolStripMenuItem
             // 
             this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
-            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(147, 26);
             this.exitToolStripMenuItem.Text = "Exit";
             this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
-            // 
-            // toolStrip1
-            // 
-            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.toolStripLabel1});
-            this.toolStrip1.Location = new System.Drawing.Point(0, 24);
-            this.toolStrip1.Name = "toolStrip1";
-            this.toolStrip1.Size = new System.Drawing.Size(1023, 25);
-            this.toolStrip1.TabIndex = 1;
-            this.toolStrip1.Text = "toolStrip1";
-            // 
-            // toolStripLabel1
-            // 
-            this.toolStripLabel1.Name = "toolStripLabel1";
-            this.toolStripLabel1.Size = new System.Drawing.Size(31, 22);
-            this.toolStripLabel1.Text = "Start";
             // 
             // statusStrip1
             // 
@@ -158,45 +141,43 @@
             this.tsProgressBar,
             this.tsslFileCounter,
             this.tsslCurrentFile});
-            this.statusStrip1.Location = new System.Drawing.Point(0, 591);
+            this.statusStrip1.Location = new System.Drawing.Point(0, 912);
             this.statusStrip1.Name = "statusStrip1";
-            this.statusStrip1.Size = new System.Drawing.Size(1023, 22);
+            this.statusStrip1.Padding = new System.Windows.Forms.Padding(2, 0, 21, 0);
+            this.statusStrip1.Size = new System.Drawing.Size(1534, 31);
             this.statusStrip1.TabIndex = 2;
             this.statusStrip1.Text = "statusStrip1";
             // 
             // tsslTimeSpan
             // 
             this.tsslTimeSpan.Name = "tsslTimeSpan";
-            this.tsslTimeSpan.Size = new System.Drawing.Size(118, 17);
-            this.tsslTimeSpan.Text = "toolStripStatusLabel2";
+            this.tsslTimeSpan.Size = new System.Drawing.Size(0, 26);
             // 
             // tsslStatus
             // 
             this.tsslStatus.Name = "tsslStatus";
-            this.tsslStatus.Size = new System.Drawing.Size(118, 17);
-            this.tsslStatus.Text = "toolStripStatusLabel3";
+            this.tsslStatus.Size = new System.Drawing.Size(0, 26);
             // 
             // tsProgressBar
             // 
             this.tsProgressBar.Name = "tsProgressBar";
-            this.tsProgressBar.Size = new System.Drawing.Size(200, 16);
+            this.tsProgressBar.Size = new System.Drawing.Size(300, 25);
             // 
             // tsslFileCounter
             // 
             this.tsslFileCounter.Name = "tsslFileCounter";
-            this.tsslFileCounter.Size = new System.Drawing.Size(118, 17);
-            this.tsslFileCounter.Text = "toolStripStatusLabel1";
+            this.tsslFileCounter.Size = new System.Drawing.Size(0, 26);
             // 
             // tsslCurrentFile
             // 
             this.tsslCurrentFile.Name = "tsslCurrentFile";
-            this.tsslCurrentFile.Size = new System.Drawing.Size(118, 17);
-            this.tsslCurrentFile.Text = "toolStripStatusLabel4";
+            this.tsslCurrentFile.Size = new System.Drawing.Size(0, 26);
             // 
             // splitContainer1
             // 
             this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.splitContainer1.Location = new System.Drawing.Point(0, 49);
+            this.splitContainer1.Location = new System.Drawing.Point(0, 31);
+            this.splitContainer1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.splitContainer1.Name = "splitContainer1";
             // 
             // splitContainer1.Panel1
@@ -206,16 +187,18 @@
             // splitContainer1.Panel2
             // 
             this.splitContainer1.Panel2.Controls.Add(this.flowLayoutPanel1);
-            this.splitContainer1.Size = new System.Drawing.Size(1023, 542);
-            this.splitContainer1.SplitterDistance = 619;
+            this.splitContainer1.Size = new System.Drawing.Size(1534, 881);
+            this.splitContainer1.SplitterDistance = 928;
+            this.splitContainer1.SplitterWidth = 6;
             this.splitContainer1.TabIndex = 3;
             // 
             // tvFindedFiles
             // 
             this.tvFindedFiles.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tvFindedFiles.Location = new System.Drawing.Point(0, 0);
+            this.tvFindedFiles.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.tvFindedFiles.Name = "tvFindedFiles";
-            this.tvFindedFiles.Size = new System.Drawing.Size(619, 542);
+            this.tvFindedFiles.Size = new System.Drawing.Size(928, 881);
             this.tvFindedFiles.TabIndex = 0;
             // 
             // flowLayoutPanel1
@@ -227,74 +210,82 @@
             this.flowLayoutPanel1.Controls.Add(this.label3);
             this.flowLayoutPanel1.Controls.Add(this.tbFindingText);
             this.flowLayoutPanel1.Controls.Add(this.btnStartSeach);
-            this.flowLayoutPanel1.Controls.Add(this.btnStop);
             this.flowLayoutPanel1.Controls.Add(this.btnPause);
+            this.flowLayoutPanel1.Controls.Add(this.btnStop);
             this.flowLayoutPanel1.Controls.Add(this.cbAllDirectories);
             this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(400, 542);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(600, 881);
             this.flowLayoutPanel1.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Location = new System.Drawing.Point(4, 0);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.Size = new System.Drawing.Size(71, 20);
             this.label1.TabIndex = 0;
             this.label1.Text = "Starpath";
             // 
             // tbFolder
             // 
             this.flowLayoutPanel1.SetFlowBreak(this.tbFolder, true);
-            this.tbFolder.Location = new System.Drawing.Point(3, 16);
+            this.tbFolder.Location = new System.Drawing.Point(4, 25);
+            this.tbFolder.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.tbFolder.Name = "tbFolder";
-            this.tbFolder.Size = new System.Drawing.Size(396, 20);
+            this.tbFolder.Size = new System.Drawing.Size(592, 26);
             this.tbFolder.TabIndex = 1;
             this.tbFolder.Text = "D:\\Temp\\";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(3, 39);
+            this.label2.Location = new System.Drawing.Point(4, 56);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(66, 13);
+            this.label2.Size = new System.Drawing.Size(100, 20);
             this.label2.TabIndex = 2;
             this.label2.Text = "File template";
             // 
             // tbTemplate
             // 
             this.flowLayoutPanel1.SetFlowBreak(this.tbTemplate, true);
-            this.tbTemplate.Location = new System.Drawing.Point(3, 55);
+            this.tbTemplate.Location = new System.Drawing.Point(4, 81);
+            this.tbTemplate.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.tbTemplate.Name = "tbTemplate";
-            this.tbTemplate.Size = new System.Drawing.Size(396, 20);
+            this.tbTemplate.Size = new System.Drawing.Size(592, 26);
             this.tbTemplate.TabIndex = 3;
             this.tbTemplate.Text = "*.*";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(3, 78);
+            this.label3.Location = new System.Drawing.Point(4, 112);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(60, 13);
+            this.label3.Size = new System.Drawing.Size(87, 20);
             this.label3.TabIndex = 4;
             this.label3.Text = "Text to find";
             // 
             // tbFindingText
             // 
-            this.tbFindingText.Location = new System.Drawing.Point(3, 94);
+            this.tbFindingText.Location = new System.Drawing.Point(4, 137);
+            this.tbFindingText.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.tbFindingText.Multiline = true;
             this.tbFindingText.Name = "tbFindingText";
-            this.tbFindingText.Size = new System.Drawing.Size(383, 115);
+            this.tbFindingText.Size = new System.Drawing.Size(572, 175);
             this.tbFindingText.TabIndex = 5;
             this.tbFindingText.Text = "0";
             // 
             // btnStartSeach
             // 
-            this.btnStartSeach.Location = new System.Drawing.Point(3, 215);
+            this.btnStartSeach.Location = new System.Drawing.Point(4, 322);
+            this.btnStartSeach.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnStartSeach.Name = "btnStartSeach";
-            this.btnStartSeach.Size = new System.Drawing.Size(75, 23);
+            this.btnStartSeach.Size = new System.Drawing.Size(112, 35);
             this.btnStartSeach.TabIndex = 6;
             this.btnStartSeach.Text = "Start";
             this.btnStartSeach.UseVisualStyleBackColor = true;
@@ -303,9 +294,10 @@
             // btnStop
             // 
             this.btnStop.Enabled = false;
-            this.btnStop.Location = new System.Drawing.Point(84, 215);
+            this.btnStop.Location = new System.Drawing.Point(244, 322);
+            this.btnStop.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnStop.Name = "btnStop";
-            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.Size = new System.Drawing.Size(112, 35);
             this.btnStop.TabIndex = 7;
             this.btnStop.Text = "Stop";
             this.btnStop.UseVisualStyleBackColor = true;
@@ -314,9 +306,10 @@
             // btnPause
             // 
             this.btnPause.Enabled = false;
-            this.btnPause.Location = new System.Drawing.Point(165, 215);
+            this.btnPause.Location = new System.Drawing.Point(124, 322);
+            this.btnPause.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnPause.Name = "btnPause";
-            this.btnPause.Size = new System.Drawing.Size(75, 23);
+            this.btnPause.Size = new System.Drawing.Size(112, 35);
             this.btnPause.TabIndex = 9;
             this.btnPause.Text = "Pause";
             this.btnPause.UseVisualStyleBackColor = true;
@@ -328,9 +321,10 @@
             this.cbAllDirectories.CheckAlign = System.Drawing.ContentAlignment.BottomRight;
             this.cbAllDirectories.Checked = true;
             this.cbAllDirectories.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.cbAllDirectories.Location = new System.Drawing.Point(246, 215);
+            this.cbAllDirectories.Location = new System.Drawing.Point(364, 322);
+            this.cbAllDirectories.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.cbAllDirectories.Name = "cbAllDirectories";
-            this.cbAllDirectories.Size = new System.Drawing.Size(90, 17);
+            this.cbAllDirectories.Size = new System.Drawing.Size(125, 24);
             this.cbAllDirectories.TabIndex = 8;
             this.cbAllDirectories.Text = "All Directories";
             this.cbAllDirectories.UseVisualStyleBackColor = true;
@@ -341,23 +335,22 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1023, 613);
+            this.ClientSize = new System.Drawing.Size(1534, 943);
             this.Controls.Add(this.splitContainer1);
             this.Controls.Add(this.statusStrip1);
-            this.Controls.Add(this.toolStrip1);
             this.Controls.Add(this.menuStrip1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
             this.Text = "File selector";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
             this.Load += new System.EventHandler(this.Form1_Load);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
-            this.toolStrip1.ResumeLayout(false);
-            this.toolStrip1.PerformLayout();
             this.statusStrip1.ResumeLayout(false);
             this.statusStrip1.PerformLayout();
             this.splitContainer1.Panel1.ResumeLayout(false);
@@ -380,8 +373,6 @@
         private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
         private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem tsmiSetFolder;
-        private System.Windows.Forms.ToolStrip toolStrip1;
-        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
         private System.Windows.Forms.StatusStrip statusStrip1;
         private System.Windows.Forms.SplitContainer splitContainer1;
         private System.Windows.Forms.TreeView tvFindedFiles;
